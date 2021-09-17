16c499cf77f91539f6163534f5d0fa472b28429b
GraceOyiza <graceoyiza13@gmail.com>
2021-09-17 22:34:29 +0100

    Add Auth0 authentication

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'figaro'
 gem 'puma', '~> 4.1'
 gem 'rubocop', '>= 1.0', '< 2.0'
+gem 'jwt'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < SecuredController
+
+    def index 
+        @users = User.all
+        render json: @users, status: :ok
+    end
+    def show
+        @user = User.first
+        render json: @user, status: :ok
+    end
+
+     def create
+        @user = User.create(user_params)
+        render json: @user, status: :created
+    end
+
+    private
+    def user_params 
+        params.permit(:username, :email, :password)
+    end
+end
