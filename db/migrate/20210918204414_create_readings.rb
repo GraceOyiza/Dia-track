9dc8d597822f40b2d88efdff4921e0369f0cb9eb
GraceOyiza <graceoyiza13@gmail.com>
2021-09-18 21:53:39 +0100

    Create Diabetic Reading Model and Migration

---
+++ db/migrate/20210918204414_create_readings.rb
@@ -0,0 +1,12 @@
+class CreateReadings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :readings do |t|
+      t.float :fasting, null: false
+      t.float :after_meal, null: false
+      t.float :bp, null: false
+      t.datetime :date, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_140625) do
+ActiveRecord::Schema.define(version: 2021_09_18_204414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "readings", force: :cascade do |t|
+    t.float "fasting", null: false
+    t.float "after_meal", null: false
+    t.float "bp", null: false
+    t.datetime "date", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
