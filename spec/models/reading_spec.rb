31e538bcefb01bbb5106edacbef41043a65e3be2
GraceOyiza <graceoyiza13@gmail.com>
2021-09-19 23:23:22 +0100

    Add Reading Model Test

--- spec/models/reading_spec.rb
+++ spec/models/reading_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Reading, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ describe 'Association' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:fasting) }
+    it { should validate_presence_of(:bp) }
+    it { should validate_presence_of(:after_meal) }
+    it { should validate_presence_of(:date) }
+  end
 end
